65de851c9b5c66c74468caeb864f92597aab523d
giovanneluna <gikalunalticg@gmail.com>
2022-06-28 16:26:01 -0300

    feat(dia 2 saga tech): adição de sistema de login

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,10 +1,19 @@
-<h1>Listagem de usuarios</h1>
+@extends('layouts.app')
+
+@section('title','Listagem do Usuário')
+
+@section('content') 
+<h1>Listagem dos usuários </h1>
+(<a href="{{route('users.create') }}">+</a>)
+
 <ul>
 @foreach ($users as $user)
     <li>
-        {{$users->name}}
-        {{$users->email}}
-
+        {{$user->name}} - 
+        {{$user->email}} 
+            <a href="{{route('users.show', $user->id)}}">Detalhes</a>
     </li>
 @endforeach
 </ul>
+    
+@endsection
